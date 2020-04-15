96b1548e7925633fb86c53ac4c44e6c95dac316e
Raul Camacho <raulc3597@gmail.com>
2020-04-15 18:59:17 -0300

    **Se agregan comentarios estilo doxygen a bluetooth.c y bluetooth.h**

--- inc/bluetooth.h
+++ inc/bluetooth.h
@@ -15,8 +15,34 @@
 #define NORMAL_ID "2"
 #define LOWBATT_ID "3"
 
+/**
+ * @brief enum para asignar un valor al tipo de ID de mensaje
+ */
+enum{
+	NO_ID = 0,
+	EMERGENCY,
+	NORMAL,
+	LOWBATT
+};
+/**
+ * funcion bluetooth_Init
+ * @brief  Inicializa puerto serial 232 de la EDUCIAA para uso de bluetooth
+ */
 void bluetooth_Init(void);
+/**
+ * funcion bluetooth_Read
+ * @brief lee el mensaje recibido por el modulo bluetooth y guarda el mensaje en msg
+ * @param msg variable donde se guardara el mensaje recibido
+ * @retrun true si se recibio un mensaje por bluetooth, false caso contrario
+ */
 bool_t bluetooth_Read(uint8_t* msg);
+/**
+ * fuincion bluetooth_Parser
+ * @brief interpreta el mensaje bluetooth guardando el id y el contenido del mensaje
+ * @param msg cadena que se desea interpretear
+ * @param pId puntero a la variable donde se guardara el id del mensaje
+ * @param receiveBuffer variable donde se guardara el mensaje que describe la situacion del paciente
+ */
 void bluetooth_Parser(uint8_t* msg, uint8_t *pId, uint8_t *receiveBuffer);
 
 #endif /* PROGRAMS_PDM_TPFINAL_INC_BLUETOOTH_H_ */
--- src/bluetooth.c
+++ src/bluetooth.c
@@ -17,16 +17,20 @@
 #define BLUETOOTH UART_232
 #define BT_BAUDRATE 9600
 #define SPC_TO_CONTENT 2
-
-enum{
-	NO_ID = 0,
-	EMERGENCY,
-	NORMAL,
-	LOWBATT
-};
-
-void get_ID(uint8_t* msg, uint8_t *pId);
-void get_Msg(uint8_t* msg, uint8_t *receiveBuffer);
+/**
+ * Funcion get_ID
+ * @brief Obtiene el ID del mensaje
+ * @param msg mensaje del que se quiere extraer el ID
+ * @param pID puntero a la variable donde se guardara el ID
+ */
+static void get_ID(uint8_t* msg, uint8_t *pId);
+/**
+ * Funcion get_Msg
+ * @brief Obtiene el mensaje que describe la necesida del paciente del mensaje recibido po BT
+ * @param msg mensaje del que se quiere extraer el mensaje del paciente
+ * @param receiveBuffer variable donde se guardara el mensaje del paciente
+ */
+static void get_Msg(uint8_t* msg, uint8_t *receiveBuffer);
 
 void bluetooth_Init(void){
 	uartConfig( BLUETOOTH, BT_BAUDRATE );
@@ -50,7 +54,7 @@ void bluetooth_Parser(uint8_t* msg, uint8_t *pId, uint8_t *receiveBuffer) {
 	get_Msg(msg, receiveBuffer);
 }
 
-void get_ID(uint8_t* msg, uint8_t *pId) {
+static void get_ID(uint8_t* msg, uint8_t *pId) {
 	uint8_t *search = strstr(msg, ID_STRING);
 	if (search != NULL) {
 		search = strstr(search, SEPARATOR);
@@ -67,7 +71,7 @@ void get_ID(uint8_t* msg, uint8_t *pId) {
 	}
 }
 
-void get_Msg(uint8_t* msg, uint8_t *receiveBuffer) {
+static void get_Msg(uint8_t* msg, uint8_t *receiveBuffer) {
 	uint8_t *search = strstr(msg, MSG_STRING);
 	if (search != NULL) {
 		search = strstr(search, SEPARATOR);
